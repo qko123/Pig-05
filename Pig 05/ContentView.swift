39b79d51ff585cc08b5845be66f1f7231c8623f8
Quinn Kozak <quinn.kozak@icloud.com>
2022-11-11 09:38:50 -0600

    made CustomText views

--- Pig 05/ContentView.swift
+++ Pig 05/ContentView.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var turnScore = 0
+    @State private var gameScore = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.opacity(0.7).ignoresSafeArea()
+            VStack {
+                Image("Pig").resizable().frame(width: 150, height: 150)
+                CustomText(text: "Pig")
+                CustomText(text: "Turn Score: \(turnScore)")
+                CustomText(text: "Game Score: \(gameScore)")
+                Spacer()
+            }
         }
-        .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct CustomText: View {
+    let text : String
+    var body: some View {
+        Text(text).font(Font.custom("Marker Felt", size: 36))
     }
 }
