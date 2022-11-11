6b96249fc0f4d20158cf3c75709d9cceab205919
Quinn Kozak <quinn.kozak@icloud.com>
2022-11-11 09:27:29 -0600

    Initial Commit

---
+++ Pig 05/Pig_05App.swift
@@ -0,0 +1,17 @@
+//
+//  Pig_05App.swift
+//  Pig 05
+//
+//  Created by Quinn Kozak on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Pig_05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
